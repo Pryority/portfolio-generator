03d09d06a1365c776d82cb51a607aea05dff844e
Matthew Pryor <matthewapryor@gmail.com>
2022-04-11 18:35:53 -0400

    feature/generate-webpage

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('================');
-
-    // Is the same as this...
-    profileDataArr.profileDataArr.forEach(profileItem => console.log(profileItem));
+const generatePage = (userName, githubName) => {
+  return `
+    Name: ${userName}
+    GitHub: ${githubName}
+  `;
 };
+console.log(generatePage('Jane', 'janehub'));
 
-printProfileData(profileDataArgs);
+
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('================');
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
